001b13b27dfcf6b452284d63bd42743925a276ee
Fabrice Weinberg <Fabrice@weinberg.me>
2016-05-30 09:51:26 +0200

    Fix remaining issues discussed in #26

--- examples/SSD1306ClockDemo/SSD1306ClockDemo.ino
+++ examples/SSD1306ClockDemo/SSD1306ClockDemo.ino
@@ -29,11 +29,13 @@
 
 // Include the correct display library
 // For a connection via I2C using Wire include
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 // For a connection via I2C using brzo_i2c (must be installed) include
+// #include <brzo_i2c.h> // Only needed for Arduino 1.6.5 and earlier
 // #include "SSD1306Brzo.h"
 // For a connection via SPI include
-// #include <SPI.h>
+// #include <SPI.h> // Only needed for Arduino 1.6.5 and earlier
 // #include "SSD1306Spi.h"
 
 // Include the UI lib
