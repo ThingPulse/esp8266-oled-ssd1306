ca4e8b7d2e7062475686181cc01251bffa67dcce
Chris Liebman <chris.l@taboola.com>
2017-11-20 15:33:50 -0800

    cleanup compiler warnings

--- OLEDDisplay.h
+++ OLEDDisplay.h
@@ -247,10 +247,10 @@ class OLEDDisplay : public Print {
     char      *logBuffer                       = NULL;
 
     // Send a command to the display (low level function)
-    virtual void sendCommand(uint8_t com) {};
+    virtual void sendCommand(uint8_t com) {(void)com;};
 
     // Connect to the display
-    virtual bool connect() {};
+    virtual bool connect() {return false;};
 
     // Send all the init commands
     void sendInitCommands();
