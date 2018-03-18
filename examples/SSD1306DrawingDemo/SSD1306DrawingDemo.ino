478e8a51c71b989b0ea7cdd4b4d4a996bc8b134a
Rotzbua <Rotzbua@users.noreply.github.com>
2018-03-18 04:05:36 +0100

    fix examples

--- examples/SSD1306DrawingDemo/SSD1306DrawingDemo.ino
+++ examples/SSD1306DrawingDemo/SSD1306DrawingDemo.ino
@@ -58,7 +58,7 @@
  // SH1106Brzo  display(0x3c, D3, D5);
 
  // Initialize the OLED display using Wire library
- SSD1306  display(GEOMETRY_128_64, 0x3c, D3, D5);
+ SSD1306  display(0x3c, D3, D5);
  // SH1106 display(0x3c, D3, D5);
 
 // Adapted from Adafruit_SSD1306
