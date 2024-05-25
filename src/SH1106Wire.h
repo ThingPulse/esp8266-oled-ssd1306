ed1feac82e5af62cf3a9c130c5e0b148f2436d16
Marcel Stör <marcelstoer@users.noreply.github.com>
2024-05-25 11:08:57 +0200

    Fix PR #404
    Fixes #410, #411

--- src/SH1106Wire.h
+++ src/SH1106Wire.h
@@ -64,12 +64,12 @@ class SH1106Wire : public OLEDDisplay {
      * ensure -1 value are specified for all 3 parameters. This can be usefull to control TwoWire with multiple
      * device on the same bus.
      *
-     * @param _address I2C Display address
-     * @param _sda I2C SDA pin number, default to -1 to skip Wire begin call
-     * @param _scl I2C SCL pin number, default to -1 (only SDA = -1 is considered to skip Wire begin call)
+     * @param address I2C Display address
+     * @param sda I2C SDA pin number, default to -1 to skip Wire begin call
+     * @param scl I2C SCL pin number, default to -1 (only SDA = -1 is considered to skip Wire begin call)
      * @param g display geometry dafault to generic GEOMETRY_128_64, see OLEDDISPLAY_GEOMETRY definition for other options
-     * @param _i2cBus on ESP32 with 2 I2C HW buses, I2C_ONE for 1st Bus, I2C_TWO fot 2nd bus, default I2C_ONE
-     * @param _frequency for Frequency by default Let's use ~700khz if ESP8266 is in 160Mhz mode, this will be limited to ~400khz if the ESP8266 in 80Mhz mode
+     * @param i2cBus on ESP32 with 2 I2C HW buses, I2C_ONE for 1st Bus, I2C_TWO fot 2nd bus, default I2C_ONE
+     * @param frequency for Frequency by default Let's use ~700khz if ESP8266 is in 160Mhz mode, this will be limited to ~400khz if the ESP8266 in 80Mhz mode
      */
     SH1106Wire(uint8_t address, int sda = -1, int scl = -1, OLEDDISPLAY_GEOMETRY g = GEOMETRY_128_64, HW_I2C i2cBus = I2C_ONE, long frequency = 700000) {
       setGeometry(g);
